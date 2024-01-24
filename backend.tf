47d7b9ef80cf61728289b7c5bd66e3daa901271a
shravansrivastava23 <146202284+shravansrivastava23@users.noreply.github.com>
2024-01-24 01:58:46 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,6 +3,9 @@ terraform {
     resource_group_name   = "myRG"
     storage_account_name  = "mytfstorage123"
     container_name        = "mytfstatecontainer"
-    key                   = "terraform.tfstate"
+    key                   = "terraform.tfstate" 
+ }
+}
+
 
     
