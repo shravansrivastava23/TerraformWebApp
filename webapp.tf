045fd213737cb1287e5c0451c9927aa88e60f280
shravansrivastava23 <146202284+shravansrivastava23@users.noreply.github.com>
2024-01-24 12:34:23 -0500

    Update webapp.tf

--- webapp.tf
+++ webapp.tf
@@ -19,3 +19,18 @@ resource "azurerm_app_service_plan" "service-plan" {
     environment = "dev"
   }
 }
+
+# Create JAVA app service
+resource "azurerm_app_service" "app-service" {
+  name = "my-awesome-app-svc"
+  location = azurerm_resource_group.dev-rg.location
+  resource_group_name = azurerm_resource_group.dev-rg.name
+  app_service_plan_id = azurerm_app_service_plan.service-plan.id
+
+site_config {
+    linux_fx_version = "TOMCAT|8.5-java11"
+  }
+tags = {
+    environment = "dev"
+  }
+}
