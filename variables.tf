c258cc5bab01c504f806f1bec70a99fddd7125f4
shravansrivastava23 <146202284+shravansrivastava23@users.noreply.github.com>
2024-01-24 01:49:27 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "resource_group_name" {
+  type = string
+  description = "RG name in Azure"
+
+}
+
+variable "location" {
+  type = string
+  description = "The location of resources in Azure"
+}
+
